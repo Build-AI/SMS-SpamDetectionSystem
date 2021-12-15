cf20dd7a04032247fcbb1fd3cc757d3bc8cd419d
rinv12 <marione1012@gmail.com>
2021-12-14 22:09:45 -0700

    done!

--- main.py
+++ main.py
@@ -1,71 +1,31 @@
-from nltk import data
-from nltk import corpus
-from nltk.util import pr
-import numpy as np
-import pandas as pd 
-import string
-import nltk
-from nltk.corpus import stopwords
-from pandas.io.parsers import read_csv 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report,confusion_matrix
-from sklearn.pipeline import Pipeline
-import naive_functions as nf
 import pandas as pd
-
-# Global variables
-pipeline = Pipeline([
-    ('bow', CountVectorizer(analyzer = nf.removes_punc)),
-    ('tfidf', TfidfTransformer()),
-    ('classifier', MultinomialNB()),
-])
-
-spam_file = "spam.csv"
+from scipy.sparse import data
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 
 def main():
-    data_frame = nf.read_csv(spam_file)
-    print(data_frame.head())
+    data_file = pd.read_csv('spam.csv', encoding = "ISO-8859-1")
+    data_file['label'] = data_file['type'].map({'ham': 'HAM', 'spam': 'SPAM'})
 
-    #describing the array of the file
-    print('\n')
-    print(data_frame.describe())
-    print('\n')
-    print(data_frame.shape)
-    print('\n')
+    file_text = data_file['text']
+    file_label = data_file['label']
 
-    spam_data = data_frame.loc[(data_frame.type == 'spam')]
-    print(spam_data.describe())
+    cv = CountVectorizer()
+    file_text = cv.fit_transform(file_text)
+    x_train, x_test, y_train, y_test = train_test_split(file_text, file_label, test_size=0.3, random_state=42)
 
-    clean_data = data_frame
-    #removing punctuation/ creating a new variable to show that we're "cleaning" the text file
-    clean_data['text'] = data_frame['text'].apply(lambda text: nf.removes_punc(text))
-    print(clean_data.head())
+    #naive bayes
+    clf = MultinomialNB()
+    clf.fit(x_train, y_train)
+    clf.score(x_test, y_test)
 
-    #calling find_total_words
-    vectorized_words = nf.find_total_words(clean_data)
-    print(vectorized_words)
+    message = ''
+    while not (message == 'quit'):
+        message = input("Enter a message: ")
+        message_data = [message]
+        vect = cv.transform(message_data).toarray()
+        predict_message = clf.predict(vect)
+        print(predict_message)
 
-    #extracting spam and ham
-    spam_data = nf.extract_spam(clean_data)
-    print(spam_data.head())
-    print(spam_data.describe())
-    print('nf')
-    ham_data = nf.extract_ham(clean_data)
-    print(ham_data)
-    print(ham_data.describe())
-
-    #not needed
-    # train_test = nf.create_data_model(clean_data)
-    # #printing lengths
-    # print('\n')
-    # print('Xtrain: ', len(train_test.x_train))
-    # print('Xtest: ', len(train_test.x_test))
-    # print('Ytrain: ', len(train_test.y_train))
-    # print('Ytest: ', len(train_test.y_test))
-
-    #calling corpus
-    nf.corpus(clean_data)
 main()
